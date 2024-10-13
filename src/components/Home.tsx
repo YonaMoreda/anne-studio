7079ccad3c111cf11497107c2ae4da2645b7d26d
Yona Moreda <yonamoreda@MacBook-Air-van-Yona.local>
2024-10-13 20:06:18 +0200

    updated home page with basic layout

---
+++ src/components/Home.tsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import '../stylesheets/Home.css'
+
+function Home() {
+
+  return (
+    <>
+      <div className='anne-header'>Anne Architectural Studio</div>
+      <div className='anne-intro'>
+        <div className='anne-whoami'>Hi, I am Anne. I am an architect, designer, planner.</div>
+      </div>
+      <div className='vert-navbar'>
+        <div className='navbar-link'>Explore</div>
+        <div className='navbar-link'>Resume</div>
+        <div className='navbar-link'>About</div>
+        <div className='navbar-link'>Contact</div>
+      </div>
+    </>
+  )
+}
+
+export default Home
