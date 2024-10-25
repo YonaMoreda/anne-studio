ec2ec1a04dbd4fdef6a23cb4754542b9073671f2
YonaMoreda <yokots3q@gmail.com>
2024-10-25 18:24:12 +0200

    added profile pic fullscreen 3d model with background clean up and cosmetic changes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,5 @@
 import '../stylesheets/Home.css'
+import ProfilePicture from  '../../public/generic_pp.png'
 
 function Home() {
 
@@ -6,13 +7,17 @@ function Home() {
     <>
       <div className='anne-header'>Anne Architectural Studio</div>
       <div className='anne-intro'>
-        <div className='anne-whoami'>Hi, I am Anne. I am an architect, designer, planner.</div>
+          <div className='anne-profile-pic-containrer'>
+              <img className="anne-profile-pic" src={ProfilePicture} alt={"profile_picture"}/>
+          </div>
+        <div className='anne-whoami'>Hi, I am Anne. I am an architect, designer, planner. Welcome to my architecture portfolio,
+            where I showcase a selection of work that reflects my passion for innovative, functional, and sustainable design.</div>
       </div>
       <div className='vert-navbar'>
         <div className='navbar-link'>Explore</div>
-        <div className='navbar-link'>Resume</div>
-        <div className='navbar-link'>About</div>
-        <div className='navbar-link'>Contact</div>
+          <div className='navbar-link'>About</div>
+          <div className='navbar-link'>Contact</div>
+          <div className='navbar-link' style={{border: '.1px solid whitesmoke', borderRadius: '10px', width: "150%", textAlign: 'center', marginTop: '10px'}}>Book now</div>
       </div>
     </>
   )
--- src/components/HouseModel.tsx
+++ src/components/HouseModel.tsx
@@ -1,71 +1,63 @@
 import * as THREE from 'three';
-import {FirstPersonControls} from "three/examples/jsm/controls/FirstPersonControls.js";
-// import earthTexture from "../../public/2k_earth_daymap.jpg"
+import {OrbitControls} from "three/examples/jsm/controls/OrbitControls.js";
 import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader.js";
-// import houseGLTFModel from "../assets/house/scene.gltf";
 import {Group} from "three/src/Three.js";
+import '../stylesheets/HouseModel.css'
+import MilkyWay from '../../public/milky-way.jpg';
 
-
-let houseModel: Group;
+let houseModelGroup: Group;
+let mixer: THREE.AnimationMixer;
 
 //Scene
 const scene = new THREE.Scene();
+scene.background = new THREE.CubeTextureLoader().setPath('textures/cubeMaps/').load([MilkyWay]);
 
 //Camera
-const camera = new THREE.PerspectiveCamera(50, window.innerWidth/window.innerHeight, 0.1, 1000)
+const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000)
 scene.add(camera)
-camera.position.z = 10
+camera.position.z = 30
 camera.lookAt(0, 0, 0)
-
-//Controls
-const clockForControls = new THREE.Clock();
-
-//Geometry
-// const geometry = new THREE.SphereGeometry(3, 64, 64)
-
-//Material
-// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-// const material = new THREE.MeshPhongMaterial({
-    // map: new THREE.TextureLoader().load(earthTexture)
-// });
-
-//Mesh
-// const earthMesh = new THREE.Mesh(geometry, material)
-// scene.add(earthMesh)
 
 //Light
 const pointLight = new THREE.PointLight(0xffffff, 300)
 pointLight.position.set(0, 10, 10)
 scene.add(pointLight)
-
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.2)
 scene.add(ambientLight)
 
 //Renderer
-let canvas = document.querySelector('#webgl')!
+const canvas = document.querySelector('#webgl')!
 const renderer = new THREE.WebGLRenderer({canvas: canvas})
-renderer.setSize(window.innerWidth/2, window.innerHeight/2)
+renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 renderer.render(scene, camera)
 renderer.setAnimationLoop(animation);
 window.addEventListener('resize', onWindowResize);
 
+// Clocks
+const clockForControls = new THREE.Clock();
+const clockForHouseMesh = new THREE.Clock();
+
 //Controls
-const controls = new FirstPersonControls(camera, renderer.domElement);
-controls.lookSpeed = 0.01;
-controls.movementSpeed = 0;
+const controls = new OrbitControls(camera, renderer.domElement);
 
 const gltfLoader = new GLTFLoader();
 gltfLoader.load('/anne-studio/src/assets/house/scene.gltf', (gltfScene) => {
-    houseModel = gltfScene.scene;
-    houseModel.position.set(-1, -1, -2);
-    houseModel.scale.set(.5, .5, .5);
-    houseModel.rotation.x = 0.3;
-    houseModel.rotation.y = Math.PI / 5;
-    scene.add(houseModel);
+    houseModelGroup = gltfScene.scene;
+    houseModelGroup.position.set(3, -5, 0);
+    houseModelGroup.scale.set(1, 1, 1);
+    houseModelGroup.rotation.x = 0;
+    houseModelGroup.rotation.y = Math.PI / 5;
+    mixer = new THREE.AnimationMixer(houseModelGroup);
+    scene.add(houseModelGroup);
 })
 
 function animation(time: DOMHighResTimeStamp) {
     pointLight.position.x = Math.sin(time / 5000) * 3;
+    if (mixer) {
+        houseModelGroup.rotation.y -= 0.001
+        mixer.update(clockForHouseMesh.getDelta());
+    }
     controls.update(clockForControls.getDelta());
     renderer.render(scene, camera);
 }
@@ -73,15 +65,15 @@ function animation(time: DOMHighResTimeStamp) {
 function onWindowResize() {
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
-    renderer.setSize(window.innerWidth/2, window.innerHeight/2);
-    controls.handleResize();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    // controls.handleResize();
 }
 
 
 function HomeModel() {
     return (
-    <>
-    </>
+        <>
+        </>
     )
 }
 
