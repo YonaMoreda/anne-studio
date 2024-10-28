4f5baaf3dd87aa3ec2f7f1f9d4cb013c6604f038
YonaMoreda <yokots3q@gmail.com>
2024-10-28 09:34:04 +0100

    move 3d model into public

--- src/components/HouseModel.tsx
+++ src/components/HouseModel.tsx
@@ -42,7 +42,7 @@ const clockForHouseMesh = new THREE.Clock();
 const controls = new OrbitControls(camera, renderer.domElement);
 
 const gltfLoader = new GLTFLoader();
-gltfLoader.load('/anne-studio/src/assets/house/scene.gltf', (gltfScene) => {
+gltfLoader.load('/anne-studio/public/assets/house/scene.gltf', (gltfScene) => {
     houseModelGroup = gltfScene.scene;
     houseModelGroup.position.set(3, -5, 0);
     houseModelGroup.scale.set(1, 1, 1);
